ca71d2e85a41da45fb5ff891896156526b9259dd
redjax <5534031+redjax@users.noreply.github.com>
2026-08-12 01:30:03 -0400

    feat(lib): Add lib/ dir

---
+++ scripts/lib/__init__.py
@@ -0,0 +1,5 @@
+"""Import everything exported in a module's `__all__ = [...]`.
+
+This makes functions, classes, and variables from these modules available
+to other modules using `from lib.xx import x`.
+"""
